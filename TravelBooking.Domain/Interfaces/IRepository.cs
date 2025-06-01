202bade74c5d78c231763a3afcb6ae05885de4ee
Abod Shashtari <abod.shashtari@gmail.com>
2025-06-01 19:50:42 +0300

    feat: create generic repository interface

---
+++ TravelBooking.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using TravelBooking.Domain.Entities;
+using TravelBooking.Domain.Shared;
+
+namespace TravelBooking.Domain.Interfaces;
+
+public interface IRepository<T> where T : EntityBase
+{
+    Task<T> GetByIdAsync(Guid id);
+    Task<PaginatedList<T>> GetAllAsync(int pageNumber, int pageSize);
+    Task<Guid> AddAsync(T entity);
+    void Delete(T entity);
+    Task <int> SaveChangesAsync(); 
+}
--- TravelBooking.Domain/TravelBookingDbContext.cs
+++ TravelBooking.Domain/TravelBookingDbContext.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TravelBooking.Domain.Entities;
 
 namespace TravelBooking.Domain;
 
 public class TravelBookingDbContext:DbContext
 {
-    
+    public DbSet<User> Users { get; set; }
+    public DbSet<TokenWhiteList> TokenWhiteList { get; set; }
+    public TravelBookingDbContext(DbContextOptions<TravelBookingDbContext> options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
 }
