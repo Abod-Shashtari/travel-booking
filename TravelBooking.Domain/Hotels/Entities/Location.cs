bcc4fe8bbe9ae4795950b478b4eec9ff6599ab9f
Abod Shashtari <abod.shashtari@gmail.com>
2025-06-05 23:07:30 +0300

    feat: create city and hotel entities

---
+++ TravelBooking.Domain/Hotels/Entities/Location.cs
@@ -0,0 +1,5 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelBooking.Domain.Hotels.Entities;
+
+public record Location([Required,Range(-90, 90)] double Latitude, [Required,Range(-180, 180)] double Longitude);
