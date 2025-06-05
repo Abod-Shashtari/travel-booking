d3b3679c973157ddc272f62b3a2a7868796602e3
Abod Shashtari <abod.shashtari@gmail.com>
2025-06-05 16:10:26 +0300

    feat: middleware for exception handling
    create middelware to handle unexpected errors/exceptions through the
    pipeline

---
+++ TravelBooking.Web/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelBooking.Web.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch(Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+    private async Task HandleExceptionAsync(
+        HttpContext httpContext,
+        Exception exception)
+    {
+        var statusCode = StatusCodes.Status500InternalServerError;
+        httpContext.Response.StatusCode = statusCode;
+
+        var problemDetails = new ProblemDetails
+        {
+            Type = exception.GetType().Name,
+            Title = "An error occurred",
+            Status = statusCode,
+            Detail = exception.Message
+        };
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails);
+    }
+}
