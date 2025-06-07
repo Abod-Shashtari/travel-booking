704eab8bfe57f466d5e405353af27447acfbf229
Abod Shashtari <abod.shashtari@gmail.com>
2025-06-07 16:45:10 +0300

    feat: implement specification pattern

--- TravelBooking.Domain/Common/Interfaces/IRepository.cs
+++ TravelBooking.Domain/Common/Interfaces/IRepository.cs
@@ -1,11 +1,13 @@
-﻿using TravelBooking.Domain.Common.Entities;
+﻿using System.Linq.Expressions;
+using TravelBooking.Domain.Common.Entities;
 
 namespace TravelBooking.Domain.Common.Interfaces;
 
 public interface IRepository<T> where T : EntityBase
 {
     Task<T?> GetByIdAsync(Guid id,CancellationToken cancellationToken = default);
-    Task<PaginatedList<T>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default);
+    Task<PaginatedList<TResult>> GetPaginatedListAsync<TResult>(Expression<Func<T, TResult>> selector,int pageNumber, int pageSize, CancellationToken cancellationToken = default);
+    Task<PaginatedList<TResult>> GetPaginatedListAsync<TResult>(ISpecification<T> specification,Expression<Func<T, TResult>> selector,int pageNumber, int pageSize, CancellationToken cancellationToken = default);
     Task<Guid> AddAsync(T entity, CancellationToken cancellationToken = default);
     Task<bool> IsExistAsync(T entity,CancellationToken cancellationToken = default);
     void Delete(T entity);
---
+++ TravelBooking.Domain/Common/Interfaces/ISpecification.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq.Expressions;
+
+namespace TravelBooking.Domain.Common.Interfaces;
+
+public interface ISpecification<T>
+{
+    Expression<Func<T, bool>>? Criteria { get; }
+}
