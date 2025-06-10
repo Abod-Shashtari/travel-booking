76a59c0f30982a042f674b00fb8209a96ca84979
Abod Shashtari <abod.shashtari@gmail.com>
2025-06-10 20:40:12 +0300

    feat: add hotel management domain entities
    - Amenity
    - Discount
    - Room
    - RoomType

---
+++ TravelBooking.Domain/Rooms/Entities/Room.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TravelBooking.Domain.Common.Entities;
+using TravelBooking.Domain.RoomTypes.Entities;
+
+namespace TravelBooking.Domain.Rooms.Entities;
+
+public class Room:AuditEntity
+{
+    [Required,MaxLength(20)]
+    public string Number { get; set; } = string.Empty;
+    [Required]
+    public Guid RoomTypeId { get; set; }
+    public RoomType? RoomType { get; set; }
+    [Required]
+    public int AdultCapacity { get; set; }
+    [Required]
+    public int ChildrenCapacity { get; set; }
+}
