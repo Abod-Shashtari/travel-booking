6ce8313d80c973a0aa043cc4f4af2047f00b89a0
Abod Shashtari <abod.shashtari@gmail.com>
2025-06-21 23:38:21 +0300

    test: unit test for images

---
+++ Application.Tests/Images/DeleteImageCommandHandlerTests.cs
@@ -0,0 +1,94 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Moq;
+using TravelBooking.Application.Common.Interfaces;
+using TravelBooking.Application.Images.DeleteImage;
+using TravelBooking.Domain.Common.Interfaces;
+using TravelBooking.Domain.Images.Entities;
+using TravelBooking.Domain.Images.Errors;
+
+namespace Application.Tests.Images;
+
+public class DeleteImageCommandHandlerTests
+{
+    private readonly IFixture _fixture;
+    private readonly Mock<IImageService> _imageService;
+    private readonly Mock<IRepository<Image>> _imageRepository;
+    private readonly DeleteImageCommandHandler _handler;
+
+    public DeleteImageCommandHandlerTests()
+    {
+        _fixture = new Fixture();
+        _fixture.Customize(new AutoMoqCustomization());
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+        _imageService = new Mock<IImageService>();
+        _imageRepository = new Mock<IRepository<Image>>();
+        _handler = new DeleteImageCommandHandler(_imageService.Object, _imageRepository.Object);
+    }
+
+    [Fact]
+    public async Task DeleteImageCommandHandler_ImageNotFound_ShouldReturnFailure()
+    {
+        // Arrange
+        var imageGuid= _fixture.Create<Guid>();
+        var command = new DeleteImageCommand(imageGuid);
+        
+        _imageRepository.Setup(r => r.GetByIdAsync(command.ImageId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Image)null);
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(ImageErrors.ImageNotFound());
+        _imageService.Verify(s => s.DeleteImageAsync(It.IsAny<string>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task DeleteImageCommandHandler_DeleteFails_ShouldReturnFailure()
+    {
+        // Arrange
+        var imageGuid= _fixture.Create<Guid>();
+        var command = new DeleteImageCommand(imageGuid);
+        var image = _fixture.Create<Image>();
+        _imageRepository.Setup(r => r.GetByIdAsync(command.ImageId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(image);
+        _imageService.Setup(s => s.DeleteImageAsync(image.Url))
+            .ReturnsAsync(false);
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(ImageErrors.ErrorWhileDeleting());
+        _imageRepository.Verify(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task DeleteImageCommandHandler_ValidRequest_ShouldDeleteAndReturnSuccess()
+    {
+        // Arrange
+        var imageGuid= _fixture.Create<Guid>();
+        var command = new DeleteImageCommand(imageGuid);
+        var image = _fixture.Create<Image>();
+        _imageRepository.Setup(r => r.GetByIdAsync(command.ImageId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(image);
+        _imageService.Setup(s => s.DeleteImageAsync(image.Url))
+            .ReturnsAsync(true);
+        _imageRepository.Setup(r => r.Delete(image));
+        _imageRepository.Setup(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(1);
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        _imageService.Verify(s => s.DeleteImageAsync(image.Url), Times.Once);
+        _imageRepository.Verify(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
---
+++ Application.Tests/Images/UploadImageCommandHandlerTests.cs
@@ -0,0 +1,113 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using TravelBooking.Application.Common.Interfaces;
+using TravelBooking.Application.Images.UploadImage;
+using TravelBooking.Domain.Common.Interfaces;
+using TravelBooking.Domain.Images.Entities;
+using TravelBooking.Domain.Images.Errors;
+
+namespace Application.Tests.Images;
+
+public class UploadImageCommandHandlerTests
+{
+    private readonly IFixture _fixture;
+    private readonly Mock<IImageService> _imageService;
+    private readonly Mock<IRepository<Image>> _imageRepository;
+    private readonly UploadImageCommandHandler _handler;
+
+    public UploadImageCommandHandlerTests()
+    {
+        _fixture = new Fixture();
+        _fixture.Customize(new AutoMoqCustomization());
+
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+        _imageService = new Mock<IImageService>();
+        _imageRepository = new Mock<IRepository<Image>>();
+        _handler = new UploadImageCommandHandler(_imageService.Object, _imageRepository.Object);
+    }
+
+    [Fact]
+    public async Task UploadImageCommandHandler_InvalidEntityType_ShouldReturnFailure()
+    {
+        // Arrange
+        var command = _fixture.Build<UploadImageCommand>()
+            .With(c => c.EntityName, "Invalid")
+            .Create();
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(ImageErrors.ImageInvalidEntityType());
+        _imageService.Verify(s => s.AddImageAsync(It.IsAny<IFormFile>(), It.IsAny<CancellationToken>()), Times.Never);
+        _imageRepository.Verify(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+    
+    [Fact]
+    public async Task UploadImageCommandHandler_ImageAlreadyExists_ShouldReturnFailureAndDeleteUploaded()
+    {
+        // Arrange
+        var validType = Enum.GetValues<EntityType>().First().ToString();
+        var command = _fixture.Build<UploadImageCommand>()
+            .With(c => c.EntityName, validType)
+            .Create();
+    
+        var imageUrl = _fixture.Create<string>();
+    
+        _imageService.Setup(s => s.AddImageAsync(command.Image, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(imageUrl);
+    
+        _imageRepository.Setup(r => r.IsExistAsync(It.IsAny<Image>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(true);
+    
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+    
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(ImageErrors.ImageAlreadyExists());
+    
+        _imageService.Verify(s => s.AddImageAsync(command.Image, It.IsAny<CancellationToken>()), Times.Once);
+        _imageService.Verify(s => s.DeleteImageAsync(imageUrl), Times.Once);
+        _imageRepository.Verify(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never);
+    }
+    
+    [Fact]
+    public async Task UploadImageCommandHandler_ValidRequest_ShouldUploadAndReturnSuccess()
+    {
+        // Arrange
+        var validType = Enum.GetValues<EntityType>().First().ToString();
+        var expectedGuid= _fixture.Create<Guid>();
+        var command = _fixture.Build<UploadImageCommand>()
+            .With(c => c.EntityName, validType)
+            .Create();
+    
+        var imageUrl = _fixture.Create<string>();
+    
+        _imageService.Setup(s => s.AddImageAsync(command.Image, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(imageUrl);
+    
+        _imageRepository.Setup(r => r.IsExistAsync(It.IsAny<Image>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(false);
+    
+        _imageRepository.Setup(r => r.AddAsync(It.IsAny<Image>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedGuid);
+    
+        _imageRepository.Setup(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(1);
+    
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+    
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        
+        _imageService.Verify(s => s.AddImageAsync(command.Image, It.IsAny<CancellationToken>()), Times.Once);
+        _imageRepository.Verify(r => r.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
