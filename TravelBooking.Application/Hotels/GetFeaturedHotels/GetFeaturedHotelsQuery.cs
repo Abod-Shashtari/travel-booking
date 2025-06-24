a826d90be30a9765cff319bc461a59cf31500753
Abod Shashtari <abod.shashtari@gmail.com>
2025-06-24 13:03:32 +0300

    feat: get featured deals of hotels

---
+++ TravelBooking.Application/Hotels/GetFeaturedHotels/GetFeaturedHotelsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TravelBooking.Application.Common.Models;
+using TravelBooking.Domain.Common;
+
+namespace TravelBooking.Application.Hotels.GetFeaturedHotels;
+
+public record GetFeaturedHotelsQuery(): IRequest<Result<PaginatedList<HotelResponse>>>;
---
+++ TravelBooking.Application/Hotels/GetFeaturedHotels/GetFeaturedHotelsQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using MediatR;
+using TravelBooking.Application.Common.Models;
+using TravelBooking.Domain.Common;
+using TravelBooking.Domain.Common.Interfaces;
+using TravelBooking.Domain.Hotels.Entities;
+
+namespace TravelBooking.Application.Hotels.GetFeaturedHotels;
+
+public class GetFeaturedHotelsQueryHandler:IRequestHandler<GetFeaturedHotelsQuery,Result<PaginatedList<HotelResponse>>>
+{
+    private readonly IRepository<Hotel> _hotelRepository;
+
+    public GetFeaturedHotelsQueryHandler(IRepository<Hotel> hotelRepository)
+    {
+        _hotelRepository = hotelRepository;
+    }
+
+    public async Task<Result<PaginatedList<HotelResponse>>> Handle(GetFeaturedHotelsQuery request, CancellationToken cancellationToken)
+    {
+        var specification = new GetFeaturedHotelsSpecification(5);
+        
+        Expression<Func<Hotel, HotelResponse>> selector = hotel => new HotelResponse(
+            hotel.Id,
+            hotel.Name,
+            hotel.Location,
+            hotel.Description,
+            hotel.City!.Name,
+            hotel.CityId,
+            hotel.OwnerId,
+            hotel.CreatedAt,
+            hotel.ModifiedAt,
+            new ImageResponse(
+                hotel.ThumbnailImageId,
+                hotel.ThumbnailImage != null ? hotel.ThumbnailImage.Url : ""
+            )
+        );
+        
+        var hotels = await _hotelRepository.GetPaginatedListAsync(
+            specification,
+            selector,
+            cancellationToken
+        );
+        
+        return Result<PaginatedList<HotelResponse>>.Success(hotels);
+    }
+}
