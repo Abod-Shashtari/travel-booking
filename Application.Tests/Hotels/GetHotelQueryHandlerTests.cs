604b135f0497e12a5b22b5c4f9938ba2ac057a2b
Abod Shashtari <abod.shashtari@gmail.com>
2025-06-24 18:02:58 +0300

    test: get recently visited hotels

--- Application.Tests/Hotels/GetHotelQueryHandlerTests.cs
+++ Application.Tests/Hotels/GetHotelQueryHandlerTests.cs
@@ -4,9 +4,11 @@ using FluentAssertions;
 using Moq;
 using TravelBooking.Application.Common.Models;
 using TravelBooking.Application.Hotels.GetHotel;
+using TravelBooking.Domain.Common;
 using TravelBooking.Domain.Common.Interfaces;
 using TravelBooking.Domain.Hotels.Entities;
 using TravelBooking.Domain.Hotels.Errors;
+using TravelBooking.Domain.UserActivity.Entites;
 
 namespace Application.Tests.Hotels;
 
@@ -14,6 +16,7 @@ public class GetHotelQueryHandlerTests
 {
     private readonly IFixture _fixture;
     private readonly Mock<IRepository<Hotel>> _hotelRepository;
+    private readonly Mock<IRepository<HotelVisit>> _hotelVisitRepository;
     private readonly GetHotelQueryHandler _handler;
 
     public GetHotelQueryHandlerTests()
@@ -22,8 +25,9 @@ public class GetHotelQueryHandlerTests
         _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
         
         _hotelRepository = new Mock<IRepository<Hotel>>();
+        _hotelVisitRepository= new Mock<IRepository<HotelVisit>>();
         
-        _handler = new GetHotelQueryHandler(_hotelRepository.Object);
+        _handler = new GetHotelQueryHandler(_hotelRepository.Object,_hotelVisitRepository.Object);
     }
 
     [Fact]
@@ -33,12 +37,35 @@ public class GetHotelQueryHandlerTests
         var command = _fixture.Create<GetHotelQuery>();
         var hotelProjection = _fixture.Create<HotelResponse>();
 
-        _hotelRepository
-            .Setup(r => r.GetByIdAsync(
+        _hotelRepository.Setup(r => r.GetByIdAsync(
                 command.HotelId,
                 It.IsAny<Expression<Func<Hotel, HotelResponse>>>(),
                 It.IsAny<CancellationToken>()))
             .ReturnsAsync(hotelProjection);
+
+        var existingHotelVisitList = _fixture.CreateMany<HotelVisit>(2).ToList();
+        var existedHotelVisitRecord = new PaginatedList<HotelVisit>(
+            existingHotelVisitList,
+            existingHotelVisitList.Count,
+            existingHotelVisitList.Count,
+            1
+        );
+        
+        _hotelVisitRepository.Setup(r => r.GetPaginatedListAsync(
+            It.IsAny<GetHotelVisitedSpecification>(),
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(existedHotelVisitRecord);
+        
+        _hotelVisitRepository.Setup(r => r.Delete(It.IsAny<HotelVisit>()));
+
+        _hotelVisitRepository.Setup(r => r.AddAsync(
+            It.IsAny<HotelVisit>(),
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(It.IsAny<Guid>());
+        
+        _hotelVisitRepository.Setup(r => r.SaveChangesAsync(
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(1);
 
         // Act
         var result = await _handler.Handle(command, CancellationToken.None);
@@ -46,6 +73,9 @@ public class GetHotelQueryHandlerTests
         // Assert
         result.IsSuccess.Should().BeTrue();
         result.Value.Should().BeEquivalentTo(hotelProjection);
+        
+        _hotelVisitRepository.Verify(r => r.Delete(It.IsAny<HotelVisit>()), 
+            Times.Exactly(existingHotelVisitList.Count));
     }
 
     [Fact]
---
+++ Application.Tests/UserActivity/GetRecentlyVisitedHotelsQueryHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq.Expressions;
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using TravelBooking.Application.Common.Models;
+using TravelBooking.Application.UserActivity.GetRecentlyVisitedHotels;
+using TravelBooking.Domain.Common;
+using TravelBooking.Domain.Common.Interfaces;
+using TravelBooking.Domain.UserActivity.Entites;
+
+namespace Application.Tests.UserActivity;
+
+public class GetRecentlyVisitedHotelsQueryHandlerTests
+{
+    private readonly IFixture _fixture;
+    private readonly Mock<IRepository<HotelVisit>> _hotelVisitRepository;
+    private readonly GetRecentlyVisitedHotelsQueryHandler _handler;
+
+    public GetRecentlyVisitedHotelsQueryHandlerTests()
+    {
+        _fixture = new Fixture();
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+        _hotelVisitRepository = new Mock<IRepository<HotelVisit>>();
+        _handler = new GetRecentlyVisitedHotelsQueryHandler(_hotelVisitRepository.Object);
+    }
+
+    [Fact]
+    public async Task GetRecentlyVisitedHotelsQueryHandler_ValidRequest_ShouldReturnPaginatedHotelResponse()
+    {
+        // Arrange
+        var query = _fixture.Create<GetRecentlyVisitedHotelsQuery>();
+
+        var hotelResponses = _fixture.CreateMany<HotelResponse>(5).ToList();
+        var paginatedResponse = new PaginatedList<HotelResponse>(
+            hotelResponses,
+            5,
+            5,
+            1
+        );
+
+        _hotelVisitRepository
+            .Setup(r => r.GetPaginatedListAsync(
+                It.IsAny<GetRecentlyVisitedHotelsSpecification>(),
+                It.IsAny<Expression<Func<HotelVisit, HotelResponse>>>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(paginatedResponse);
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+        result.Value.Data.Should().BeEquivalentTo(paginatedResponse.Data);
+        result.Value.TotalCount.Should().Be(paginatedResponse.TotalCount);
+        result.Value.PageSize.Should().Be(paginatedResponse.PageSize);
+        result.Value.CurrentPage.Should().Be(paginatedResponse.CurrentPage);
+    }
+}
