d7d523d67f3671e496c88639ba1816109e07c246
Abod Shashtari <abod.shashtari@gmail.com>
2025-06-28 23:17:42 +0300

    feat: email service

---
+++ TravelBooking.Application/Common/Interfaces/IEmailSender.cs
@@ -0,0 +1,8 @@
+﻿using TravelBooking.Application.Common.Models;
+
+namespace TravelBooking.Application.Common.Interfaces;
+
+public interface IEmailSender
+{
+    Task SendEmailAsync(string email, string subject, string message, PdfResult? pdfResult = null, CancellationToken cancellationToken = default);
+}
