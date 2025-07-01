de8d919702f48732c8ea83fa03d2606bff819846
Abod Shashtari <abod.shashtari@gmail.com>
2025-07-01 12:31:01 +0300

    doc: write documentation on endpoints

--- TravelBooking.Web/Controllers/PaymentController.cs
+++ TravelBooking.Web/Controllers/PaymentController.cs
@@ -20,6 +20,21 @@ public class PaymentController:ControllerBase
         _sender = sender;
     }
 
+    /// <summary>
+    /// Creates a payment intent for a specified booking.
+    /// </summary>
+    /// <param name="bookingId">The ID of the booking to create a payment intent for.</param>
+    /// <returns>Returns the payment intent details if successful.</returns>
+    /// <response code="200">Payment intent created successfully.</response>
+    /// <response code="400">Invalid request or booking state.</response>
+    /// <response code="401">Unauthorized user.</response>
+    /// <response code="404">Booking not found.</response>
+    /// <response code="409">User not allowed</response>
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpPost("bookings/{bookingId}/create-payment-intent")]
     public async Task<IActionResult> CreatePaymentIntent(Guid bookingId)
     {
