cfdeb9ea8357858a08e0895d25c4430aadc59748
Abod Shashtari <abod.shashtari@gmail.com>
2025-07-05 10:10:57 +0300

    perf: add indexes to improve query performance

---
+++ TravelBooking.Infrastructure/Configurations/RoomConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TravelBooking.Domain.Rooms.Entities;
+
+namespace TravelBooking.Infrastructure.Configurations;
+
+public class RoomConfiguration:IEntityTypeConfiguration<Room>
+{
+    public void Configure(EntityTypeBuilder<Room> builder)
+    {
+        builder.HasIndex(r=>r.AdultCapacity);
+        builder.HasIndex(r=>r.ChildrenCapacity);
+        builder.HasIndex(r => new { r.AdultCapacity, r.ChildrenCapacity });
+    }
+}
